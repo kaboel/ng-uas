90ad0a544c5cac949d669bc0350a6efb01660a94
Faiq Allam <faiq.kaboel@gmail.com>
2019-05-19 10:44:30 +0700

    Add sign-in component

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../core/services/auth.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {faGoogle} from '@fortawesome/free-brands-svg-icons/faGoogle';
 import {faExclamationCircle} from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
 
@@ -10,13 +11,29 @@ import {faExclamationCircle} from '@fortawesome/free-solid-svg-icons/faExclamati
 })
 export class SignInComponent implements OnInit {
 
+    loginForm: FormGroup;
+
     faGoogle = faGoogle;
     faExclamationCircle = faExclamationCircle;
 
-    constructor(public authService: AuthService) {
+    constructor(
+        public authService: AuthService,
+        public formBuilder: FormBuilder
+    ) {
     }
 
     ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            email: ['', [
+                Validators.required,
+                Validators.email
+            ]],
+            pwd: ['', [
+                Validators.required,
+                Validators.minLength(6)
+            ]],
+        });
     }
 
+    get key() { return this.loginForm.controls; }
 }
