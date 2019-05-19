92cc834fe9e956e3c3cf41d6a5d0423301868354
Faiq Allam <faiq.kaboel@gmail.com>
2019-05-20 00:20:56 +0700

    Add forgot-password component

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../core/services/auth.service';
-import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {faExclamationCircle} from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
 
 @Component({
     selector: 'app-forgot-password',
@@ -9,10 +10,28 @@ import {Router} from '@angular/router';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-    constructor(public authService: AuthService) {
+    emailForm: FormGroup;
+    faExclamationCircle = faExclamationCircle;
+
+    constructor(
+        public authService: AuthService,
+        public formBuilder: FormBuilder
+    ) {
     }
 
     ngOnInit() {
+        this.emailForm = this.formBuilder.group({
+            email: ['', [
+                Validators.required,
+                Validators.email,
+            ]]
+        });
+    }
+
+    get email() { return this.emailForm.get('email'); }
+
+    onSubmit() {
+        this.authService.forgotPassword(this.email.value);
     }
 
 }
